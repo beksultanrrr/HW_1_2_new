675e43aa1ed1cf197acd5ce28b813a989cdf5917
Beksultan <ramazanov.beksultan1902@gmail.com>
2022-07-18 17:00:06 +0500

    Initial Commit

---
+++ HW_1_2_new/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_1_2_new
+//
+//  Created by beksultan on 18.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
